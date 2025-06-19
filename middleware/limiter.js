f5879c4fcb7a7b268541d2a3c0133725cd6f318b
Vladyslav Petrenko <legionvlad@icloud.com>
2025-06-19 19:27:56 +0200

    test

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,  // 1 минута
-  max: 30,               // максимум 30 запросов
+  max: 60,             // максимум 30 запросов
   message: 'Слишком много запросов. Попробуйте позже.',
 });
 

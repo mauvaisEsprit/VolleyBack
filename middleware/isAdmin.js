a36e55aa0d8efe9a4f8ac306382c371b41b5b426
Vladyslav Petrenko <legionvlad@icloud.com>
2025-06-19 14:10:31 +0200

    test

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,9 +1,20 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = function isAdmin(req, res, next) {
   const authHeader = req.headers.authorization;
-    if (!authHeader) return res.status(401).json({ message: 'Нет токена' });
+  if (!authHeader) return res.status(401).json({ message: 'Нет токена' });
 
-    const token = authHeader.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'Нет токена' });
+  const token = authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Нет токена' });
 
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role !== 'admin') {
+      return res.status(403).json({ message: 'Доступ запрещён' });
+    }
+    req.user = decoded; // чтобы дальше можно было использовать данные пользователя
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Неверный токен' });
+  }
 };
